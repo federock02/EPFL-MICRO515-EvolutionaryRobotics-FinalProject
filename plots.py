8a4b2eeaa845145f22e139f6ea101c2e2f2bec5b
AlexZanetta <alex.zanetta@epfl.ch>
2025-05-22 17:06:41 +0200

    Fitness plot

---
+++ plots.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+PATH = "results/Ant_custom/multi/full_f.npy"
+
+data = np.load(PATH)
+
+generation, pop_size, opti_param = data.shape
+
+avg_param = []
+
+for i in range(generation):
+    avg_param.append(np.mean(data[i], axis=0))
+
+plt.plot(avg_param)
+plt.title("Average fitness evolution per parameters")
+plt.xlabel("Generation")
+plt.ylabel("Average fitness")
+plt.legend(['Forward speed', 'Control costs'])
+plt.show()
