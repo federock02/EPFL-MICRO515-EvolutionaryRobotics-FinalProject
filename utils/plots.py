2dca7fc309ee43c6852fcb39b6ad9fdc370db334
AlexZanetta <alex.zanetta@epfl.ch>
2025-06-08 10:23:41 +0200

    CMAES2

--- utils/plots.py
+++ utils/plots.py
@@ -31,11 +31,18 @@ data = np.load(PATH)
 generation, pop_size = data.shape
 
 avg_param = []
+std_param = []
 
 for i in range(generation):
     avg_param.append(np.mean(data[i], axis=0))
+    std_param.append(np.std(data[i], axis=0))
 
-plt.plot(avg_param)
+avg_param = np.array(avg_param)
+std_param = np.array(std_param)
+
+
+plt.plot(avg_param, label='Fitness velocity')
+plt.fill_between(range(len(avg_param)), avg_param - std_param, avg_param + std_param, alpha=0.2)
 plt.title("Average fitness evolution")
 plt.xlabel("Generation")
 plt.ylabel("Average fitness")
